472c722962b7c53c2c23a4767dad32adbd6e3833
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-30 20:09:26 +0000

    changed app to make ready for movies components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import * as styles from './App.css'
-import Hello from './Hello';
-import CounterButton from './CounterButton';
-import NumberBoard from './NumberBoard';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Movies from './pages/Movies';
 
-export default class App extends React.Component<{},{}> {
+export default class App extends React.Component {
   render() {
-    return  <div className={styles.template}>
-              <Hello name="Mutta"></Hello>
-              <CounterButton/>
-              <NumberBoard max={5} min={-5}/>
-            </div>
+    return  <Router>
+              <div>
+                <Header/>
+                <Route exact path="/" component={Home}/>
+                <Route path="/home" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/movies" component={Movies}/>
+              </div>
+            </Router>
   }
 }
