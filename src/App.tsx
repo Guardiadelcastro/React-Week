d77262bcc3f4c75cf9167f44ddde740de913d580
Alejandro Castro <guardiadelcastro@gmail.com>
2018-12-04 18:03:53 +0000

    added first config of redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react';
 import * as styles from './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import * as Redux from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer, { AppState } from './reducers'
+import { decrementCounter, incrementCounter } from './actions';
 import Home from './pages/Home';
 import About from './pages/About';
 import Movies from './pages/Movies';
 
+const store: Redux.Store<AppState> = Redux.createStore(rootReducer);
+
+function increment(){
+  store.dispatch(incrementCounter(1));
+}
+
+function decrement() {
+  store.dispatch(decrementCounter(1))
+}
 export default class App extends React.Component {
   render() {
-    return  <Router>
-              <div>
-                <Header/>
-                <Route exact path="/" component={Home}/>
-                <Route path="/home" component={Home}/>
-                <Route path="/about" component={About}/>
-                <Route path="/movies" component={Movies}/>
-              </div>
-            </Router>
+    return <Provider store={store}>
+      <Router>
+        <div>
+          <Header/>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>decrement</button>
+          <Route exact path="/" component={Home}/>
+          <Route path="/home" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/movies" component={Movies}/>
+        </div>
+      </Router>
+    </Provider>
   }
 }
